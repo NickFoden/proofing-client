a115ddb0a33f85474822729733c78a47e760a386
Nick Foden <nickfoden@gmail.com>
2017-08-30 13:55:17 +0100

    Added dropzone for upload

--- src/Components/PhotoUploadForm.js
+++ src/Components/PhotoUploadForm.js
@@ -1,28 +1,55 @@
 import React from 'react';
 import './PhotoUploadForm.css';
-import API_BASE_URL from '../config';
+import Dropzone from 'react-dropzone'
+import sha1 from 'sha1'
+import superagent from 'superagent'
 
 class PhotoUploadForm extends React.Component {
     constructor(props){
         super(props);
     }
     
+    uploadFile(files){
+        console.log('uploadFile: ')
+        const image = files[0]
 
-    handleSubmit(e){
-        e.preventDefault();
-        fetch(API_BASE_URL, {
-            method: 'POST',
-            body: file
-        });
+        const cloudName = 'proofer'
+        const url = 'https://api.cloudinary.com/v1_1/'+cloudName+'/image/upload'
+
+        const timestamp = Date.now()/1000
+        const uploadPreset = 'on6jfv7m'
+
+        const paramStr = 'timestamp='+timestamp+'&upload_preset='+uploadPreset+'dW9zgguPfWSrOwP8mQ2AyCMYu30'
+        const signature = sha1(paramStr)
+
+        const params = {
+            'api_key': '821318977471469',
+            'timestamp': timestamp,
+            'upload_preset': uploadPreset,
+            'signature': signature
+        }
+    
+        let uploadRequest = superagent.post(url)
+        uploadRequest.attach('file', image)
+
+        Object.keys(params).forEach((key) => {
+            uploadRequest.field(key, params[key])
+        })
+
+        uploadRequest.end((err, resp) => {
+            if (err){
+                alert(err)
+                return
+            }
+            console.log('UPLOAD COMPLETE: '+JSON.stringify(resp.body))
+        })
     }
+
     render(){
 
     return (
         <form className="upload" encType="multipart/form-data" onSubmit={this.handleSubmit}>
-            <div className="field">
-                <label>Image</label>
-                <input id="upload" className="input" name="file" type="file" multiple="multiple" />
-            </div>
+            <Dropzone onDrop={this.uploadFile.bind(this)} />
             <div className="field">
                 <label>Title</label>
                 <input className="input" name="title" type="text" placeholder="(Optional)" />
