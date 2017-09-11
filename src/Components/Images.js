0155ee51fbcbe8a7ab1c37106a7072697f5d84c2
Nick Foden <nickfoden@gmail.com>
2017-09-11 16:12:25 -0400

    Changed id to be variable for li items

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -4,12 +4,12 @@ import Proofing from './Proofing'
 
 export function Images (props) {
     const testImage = "http://res.cloudinary.com/proofer/image/upload/v1502816770/gmssxsrnfd5i7rivupjx.jpg"
-        
+    const testStatus = "imgYes"    
     return (
         <div id="Images">
             <ul> 
                 <li>
-                    <img className="rawImage" id="imgYes" src={testImage} alt="imageToBeApproved" />
+                    <img className="rawImage" id={testStatus} src={testImage} alt="imageToBeApproved" />
                     <Proofing />
                 </li>
             </ul>
