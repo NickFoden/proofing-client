f873d4610b2fafeab95020b4557c7cbb92bdaeb7
Nick Foden <nickfoden@gmail.com>
2017-12-04 22:09:13 -0500

    Added or statement to config for env variable.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Settings} from './Components/Settings';
+import Home from '../src/Components/Home';
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Settings />, div);
+  ReactDOM.render(<Home/>, div);
 });
