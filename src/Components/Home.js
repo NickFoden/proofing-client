e37cc9d90b602d33d221be5e4b0a32b36b8ef806
Nick Foden <nickfoden@gmail.com>
2017-12-04 21:57:13 -0500

    Added styling to home component.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Form.css';
 
 const Home = (props) => {
 
     return  (
-        <div>
+        <div className="welcome-div">
             <h2>Welcome to Proofing</h2>
             <p>An app where you can upload photos and vote which ones make the cut</p>
             <p> Note -> Put a sample image in action here one approved one not</p>
