e3df72d3c7399f26bb5c1e799b661e24c3e49eca
Nick Foden <nickfoden@gmail.com>
2018-02-04 13:12:28 -0500

    Manual set of api url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080';
+// export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080';
 
-// export const API_BASE_URL = 'https://proofing-server.herokuapp.com';
+export const API_BASE_URL = 'https://proofing-server.herokuapp.com';
