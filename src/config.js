1ccecbbb3602a4f563af6d0749e62bcd086dd02b
Nick Foden <nickfoden@gmail.com>
2018-02-04 11:18:33 -0500

    Require dot env

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
+require('dotenv').config();
+
 export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080';
 // export const API_BASE_URL = 'https://proofing-server.herokuapp.com';
